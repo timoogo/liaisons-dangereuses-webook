c82c94ac577c7677ea7a7b5ad48a7306af97d94c
Timothée Gaultier <tiimogo@gmail.com>
2022-03-01 18:47:44 +0100

    initial intalls before adding hot reload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: {
+    content: ['./templates/**/*.html.twig']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
